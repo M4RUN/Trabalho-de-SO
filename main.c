3ab58bd4b68b90fbc1460bc0e7458a820ad36258
Marcélia Almeida <almeidamarcelia7@gmail.com>
2021-01-26 11:35:20 -0300

    Initiating
    Initiating the repository

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: MRU
+ *
+ * Created on 26 de Janeiro de 2021, 11:04
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
