10d2be08dfbfd17bff90b63103a19617a3cd23d2
Marcélia Almeida <almeidamarcelia7@gmail.com>
2021-02-19 13:51:06 -0300

    Pipeline
    Método que controla o uso de n pipes em cima de um array de comandos

--- main.c
+++ main.c
@@ -13,11 +13,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <wait.h>
 
 /*
  * 
  */
+
+//Compartilha pipes
+//Percorre o array de comandos num while e os processa por meio de pipes
+static void pipeline(char ***cmd) {
+    int fd[2];
+    pid_t pid;
+    int fdd = 0; 
+
+    while (*cmd != NULL) {
+        pipe(fd); //Começa o processo de pipe
+        if ((pid = fork()) == -1) {
+            perror("fork");
+            exit(1);
+        } else if (pid == 0)  
+            dup2(fdd, 0);
+            if (*(cmd + 1) != NULL) {
+                dup2(fd[1], 1);
+            }
+            close(fd[0]);
+            execvp((*cmd)[0], *cmd);
+            exit(1);
+        } else {
+            wait(NULL); 
+            close(fd[1]);
+            fdd = fd[0];
+            cmd++;
+        }
+    }
+}
+
 int main(int argc, char** argv) {
+    
+    
 
     return (EXIT_SUCCESS);
 }
--- nbproject/Makefile-Release.mk
+++ nbproject/Makefile-Release.mk
@@ -56,11 +56,11 @@ LDLIBSOPTIONS=
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/trabalhoso.exe
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/trabalho-de-so.exe
 
-${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/trabalhoso.exe: ${OBJECTFILES}
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/trabalho-de-so.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
-	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/trabalhoso ${OBJECTFILES} ${LDLIBSOPTIONS}
+	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/trabalho-de-so ${OBJECTFILES} ${LDLIBSOPTIONS}
 
 ${OBJECTDIR}/main.o: main.c
 	${MKDIR} -p ${OBJECTDIR}
