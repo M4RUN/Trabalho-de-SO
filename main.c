6ae66af5ffeac605ac3e6509fb8cb6f15e116139
Marcélia Almeida <almeidamarcelia7@gmail.com>
2021-02-19 14:34:39 -0300

    Metodo para execucao sem pipe
    Criado o metodo para executar uma linha de comando sem pipe

--- main.c
+++ main.c
@@ -22,17 +22,18 @@
 
 //Compartilha pipes
 //Percorre o array de comandos num while e os processa por meio de pipes
+
 static void pipeline(char ***cmd) {
     int fd[2];
     pid_t pid;
-    int fdd = 0; 
+    int fdd = 0; /* Backup */
 
     while (*cmd != NULL) {
-        pipe(fd); //Começa o processo de pipe
+        pipe(fd); /* Sharing bidiflow */
         if ((pid = fork()) == -1) {
-            perror("fork");
+            perror("Erro! Não foi possível fazer fork!");
             exit(1);
-        } else if (pid == 0)  
+        } else if (pid == 0) {
             dup2(fdd, 0);
             if (*(cmd + 1) != NULL) {
                 dup2(fd[1], 1);
@@ -41,7 +42,7 @@ static void pipeline(char ***cmd) {
             execvp((*cmd)[0], *cmd);
             exit(1);
         } else {
-            wait(NULL); 
+            wait(NULL); /* Collect childs */
             close(fd[1]);
             fdd = fd[0];
             cmd++;
@@ -49,9 +50,29 @@ static void pipeline(char ***cmd) {
     }
 }
 
+//Para quando o usuário entra uma linha sem pipes
+void execucaoSemPipes(char** cmd) {
+    // Fazendo o fork
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        perror("Erro! Não foi possível fazer fork!");
+        return;
+    } else if (pid == 0) {
+        if (execvp(cmd[0], cmd) < 0) {
+            perror("Erro! Não foi possível executar o comando!");
+        }
+        exit(0);
+    } else {
+        // Espera o filho fechar 
+        wait(NULL);
+        return;
+    }
+}
+
 int main(int argc, char** argv) {
-    
-    
+
+
 
     return (EXIT_SUCCESS);
 }
