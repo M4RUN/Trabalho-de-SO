199b6aa419bd3248f46918620cae52263c7c2270
AlefIF <aleffaria99@gmail.com>
2021-02-19 17:53:06 -0300

    choro

--- main.c
+++ main.c
@@ -10,36 +10,100 @@
 #define WRITE_END 1
 
 #define MAXNUMLETRAS 100 // numero maximo de letras
+#define MAXCOMANDOS 10 // numero maximo de letras
 
 //cria o pipe
-int criaPipe(char** comando, int n);
+static void pipeline(char ***cmd);
 //quebra o texto
-void quebra(char* texto);
+void quebraEmComandos(char* texto);
+void quebraEmPalavras(char** texto, int qtd);
 
 int main() {
-    
+
     char texto3[MAXNUMLETRAS];
     fgets(texto3, MAXNUMLETRAS + 1, stdin);
-    quebra(texto3);
+    quebraEmComandos(texto3);
+
     return (0);
 }
 
-int criaPipe(char** comando, int n) {
-    printf("Fork-Pipe: %s\n", comando[n]);
-    return 0;
-}
+static void pipeline(char ***cmd) {
+    int fd[2];
+    pid_t pid;
+    int fdd = 0;
 
-void quebra(char* texto) {
-    char *argv[256];
-    int argc = 0;
-    char * token = strtok(texto, "\n |");
-    while (token != NULL) {              
-        argv[argc] = token;
-        criaPipe(argv, argc);
-        argc++;
-        token = strtok(NULL, " \n |");
+    while (*cmd != NULL) {
+        pipe(fd); 
+        if ((pid = fork()) == -1) {
+            perror("fork");
+            exit(1);
+        } else if (pid == 0) {
+            dup2(fdd, 0);
+            if (*(cmd + 1) != NULL) {
+                dup2(fd[1], 1);
+            }
+            close(fd[0]);
+            execvp((*cmd)[0], *cmd);
+            exit(1);
+        } else {
+            wait(NULL);
+            close(fd[1]);
+            fdd = fd[0];
+            cmd++;
+        }
     }
 }
+
+void quebraEmPalavras(char** texto, int qtdC) {
+    char *comandos2[qtdC][MAXNUMLETRAS];
+    int qtdP = 0, aux = 0;
+
+    char * token;
+
+
+
+    //separando cada palavra de cada comando
+    for (int i = 0; i < qtdC; i++) {
+        token = strtok(texto[i], " \n");
+        comandos2[i][aux] = token;
+        while (token != NULL) {
+            qtdP++;
+            aux++;
+            token = strtok(NULL, " \n");
+            comandos2[i][aux] = token;
+        }
+        aux = 0;
+    }
+    //Printando
+    for (int i = 0; i < qtdC; i++) {
+        printf("Comando:%d", i);
+        printf("\n");
+        for (int j = 0; j < qtdP; j++) {
+            printf("Palavra %d: ", j);
+            printf("%s ", comandos2[i][j]);
+        }
+        printf("\n");
+    }
+    
+    pipeline(&comandos2);
+}
+
+void quebraEmComandos(char* texto) {
+
+    char *comandos[MAXCOMANDOS];
+    int qtd = 0;
+    char * token = strtok(texto, "|");
+    comandos[qtd] = token;
+    //separando os comandos
+    while (token != NULL) {
+        qtd++;
+        token = strtok(NULL, "|");
+        comandos[qtd] = token;
+    }
+    quebraEmPalavras(comandos, qtd);
+}
+
+
 
 
 
