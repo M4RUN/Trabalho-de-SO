4d96ff2a6f9f118bb3a1f12e874a754e386acbef
Marcélia Almeida <almeidamarcelia7@gmail.com>
2021-02-26 08:48:26 -0300

    FileRedirect
    Faz o redirecionamento para arquivo nos pipes

--- main.c
+++ main.c
@@ -15,10 +15,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <wait.h>
-
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 /*
  * 
  */
+//Liga inicio de pipe a um arquivo
+
+void inIO(char *fileName) {
+    int in = open(fileName, O_RDONLY);
+    dup2(in, 0);
+    close(in);
+}
+
+/**
+ * Redirects stdout to a file.
+ * 
+ * @param fileName the file to redirect to
+ */
+//Liga fim de pipe a um arquivo
+
+void outIO(char *fileName) {
+    int out = open(fileName, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+    dup2(out, 1);
+    close(out);
+}
 
 //Compartilha pipes
 //Percorre o array de comandos num while e os processa por meio de pipes
@@ -26,54 +50,104 @@
 static void pipeline(char ***cmd) {
     int fd[2];
     pid_t pid;
-    int fdd = 0; /* Backup */
+    int fdd = 0;
+    int in = 0, out = 0; //Tokens para verificar se ha redirecionamento
+    int i = 0;
+    char* filename;
+    char* cmds;
 
     while (*cmd != NULL) {
-        pipe(fd); /* Sharing bidiflow */
-        if ((pid = fork()) == -1) {
-            perror("Erro! Não foi possível fazer fork!");
-            exit(1);
-        } else if (pid == 0) {
-            dup2(fdd, 0);
-            if (*(cmd + 1) != NULL) {
-                dup2(fd[1], 1);
+        in = 0;
+        out = 0;
+        while (cmd[i] != NULL) {
+            if ((strcmp(cmd[i], ">") == 1)) {
+                 for (int j = 0; j < i; ++i) {
+                    strcpy(cmds[j], cmd[]);                 
+                }
+                out = 1;
+                strcpy(filename,cmd[i+1]);
+                break;
+            } else if (strcmp(cmd[i], "<") == 1) {
+                for (int j = 0; j < i; ++i) {
+                    strcpy(cmds[j], cmd[]);
+                    
+                }
+                    strcpy(filename,cmd[i+1]);
+                    in = 1;
+                    break;
+                }
+                i++;
             }
-            close(fd[0]);
-            execvp((*cmd)[0], *cmd);
-            exit(1);
+            pipe(fd);
+            if ((pid = fork()) == -1) {
+                perror("Erro! Não foi possível fazer fork!");
+                exit(1);
+            } else if (pid == 0) {
+                if (in) {
+                    inIO(filename);
+                    execvp((*cmds)[0], *cmds);
+                    exit(1);
+                } else if (out) {
+                    outIO(filename);
+                    execvp((*cmds)[0], *cmds);
+                    exit(1);
+                } else {
+                    dup2(fdd, 0);
+                    if (*(cmd + 1) != NULL) {
+                        dup2(fd[1], 1);
+                    }
+                    close(fd[0]);
+                    execvp((*cmd)[0], *cmd);
+                    exit(1);
+                }
+                
+            } else {
+
+                wait(NULL); /* Collect childs */
+                close(fd[1]);
+                fdd = fd[0];
+                cmd++;
+            }
+        }
+    }
+
+    //Para fazer cd
+
+    void cd(char *cmd) {
+
+        if(cmd[1]==NULL){
+            printf("Endereco de diretorio invalido!");
+        }else{
+        chdir(cmd[1]);
+        }
+    }
+
+    //Para quando o usuário entra uma linha sem pipes
+
+    void execucaoSemPipes(char** cmd) {
+        // Fazendo o fork
+        pid_t pid = fork();
+
+        if (pid == -1) {
+            perror("Erro! Não foi possível fazer fork!");
+            return;
+        } else if (pid == 0) {
+            if (execvp(cmd[0], cmd) < 0) {
+                perror("Erro! Não foi possível executar o comando!");
+            }
+            exit(0);
         } else {
-            wait(NULL); /* Collect childs */
-            close(fd[1]);
-            fdd = fd[0];
-            cmd++;
+            // Espera o filho fechar 
+            wait(NULL);
+
+            return;
         }
     }
-}
 
-//Para quando o usuário entra uma linha sem pipes
-void execucaoSemPipes(char** cmd) {
-    // Fazendo o fork
-    pid_t pid = fork();
+    int main(int argc, char** argv) {
 
-    if (pid == -1) {
-        perror("Erro! Não foi possível fazer fork!");
-        return;
-    } else if (pid == 0) {
-        if (execvp(cmd[0], cmd) < 0) {
-            perror("Erro! Não foi possível executar o comando!");
-        }
-        exit(0);
-    } else {
-        // Espera o filho fechar 
-        wait(NULL);
-        return;
+
+
+        return (EXIT_SUCCESS);
     }
-}
-
-int main(int argc, char** argv) {
-
-
-
-    return (EXIT_SUCCESS);
-}
 
