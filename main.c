b69ab27de4e2d01aa9d551b605c6ec8d3f39de0c
AlefIF <aleffaria99@gmail.com>
2021-03-19 09:28:18 -0300

    Corrigindo alguns comentários, excluindo includes nao usados

--- main.c
+++ main.c
@@ -2,39 +2,27 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <errno.h>
 #include <fcntl.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
-#include <ctype.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-#define MAX_NUM_LETRAS 1024 // The maximum length command
+#define MAX_NUM_LETRAS 100 // O maximo de letras por comando inserido
 
-/**
- *funcao para quando receber o <
- */
+//funcao de arquivo executada quando ler um char <
 void ler(char *localArquivo) {
     int arquivo = open(localArquivo, O_RDONLY);
     dup2(arquivo, 0);
     close(arquivo);
 }
 
-/**
- * funcao para quando receber o >
- */
+//funcao de arquivo executada quando ler um char >
 void salvar(char *localArquivo) {
     int arquivo = open(localArquivo, O_WRONLY | O_TRUNC | O_CREAT, 0600);
     dup2(arquivo, 1);
     close(arquivo);
 }
 
-/**
- * forkas a command.
- * 
- * @param *comando[] the comando to forka
- */
+//Realiza um fork quando ler um char |
 int forka(char *comando[]) {
     pid_t pid;
     if (strcmp(comando[0], "sair") != 0) {
@@ -53,11 +41,7 @@ int forka(char *comando[]) {
     return 0;
 }
 
-/**
- * Creates a pipe.
- * 
- * @param comando [description]
- */
+//Cria um pipe
 int criaPipe(char *comando[]) {
     int retorno = 0;
     int fd[2];
@@ -77,18 +61,13 @@ int criaPipe(char *comando[]) {
     return retorno;
 }
 
-/*
- * Recebe o texto do usuario e o formata em um vetor de char
- * cada palavra fica em um index
- * Possui um tratamento para os simbolos <, >, |
- * retorna um vetor formatado.
- */
+//Recebe o texto do usuario e o formata em um vetor de char
 char * formatar(char *textoUser) {
     int i;
     int j = 0;
     char *formatado = (char *) malloc((MAX_NUM_LETRAS * 2) * sizeof (char));
 
-    // add spaces around special characters
+    // adiciona espacos entre os caracteres especiais
     for (i = 0; i < strlen(textoUser); i++) {
         if (textoUser[i] != '>' && textoUser[i] != '<' && textoUser[i] != '|') {
             formatado[j++] = textoUser[i];
@@ -100,7 +79,7 @@ char * formatar(char *textoUser) {
     }
     formatado[j++] = '\0';
 
-    // add null to the end    
+    // adcionando null no final
     char *fim;
     fim = formatado + strlen(formatado) - 1;
     fim--;
@@ -109,21 +88,18 @@ char * formatar(char *textoUser) {
 }
 
 int main(void) {
-    char textoUser[MAX_NUM_LETRAS]; // Recebe os parametros do usuario   
+    char textoUser[MAX_NUM_LETRAS]; // Recebe os parametros do usuario
     char *token; // recebe o texto do usuario formatado de forma correta
-    char *comando[MAX_NUM_LETRAS]; //recebe um comando 
+    char *comando[MAX_NUM_LETRAS]; //recebe um comando
     char *palavra; //recebe uma paralavra do token
     int retorno = 0; //variavel de controle do loop
     int i = 0; //variavel auxiliar para a posicao dos comandos
     while (retorno == 0) {
         printf("$ ");
-        //fflush(stdout);    
         fgets(textoUser, MAX_NUM_LETRAS, stdin);
-
         token = formatar(textoUser);
         palavra = strtok(token, " ");
         i = 0;
-
         while (palavra) {
             if (*palavra == '|') {
                 comando[i] = NULL;
@@ -145,7 +121,7 @@ int main(void) {
             }
             palavra = strtok(NULL, " ");
         }
-        if (i != 0) {//caso o usurario digite apenas enter, o programa nao gera exception na hora do fork
+        if (i != 0) {//caso o usurario digite apenas enter, o programa nao gera exception
             comando[i] = NULL;
             retorno = forka(comando);
         }
